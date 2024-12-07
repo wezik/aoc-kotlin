2c442050cc8869080917c0fac36b135f486c68b1
Dominik <wezik@users.noreply.github.com>
2024-12-07 22:14:57 +0100

    Add simple shell run script

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+echo "🎅 Welcome to the advent of code 2024 solver 🎄"
+read -p "Input: [1-24] day / [0] run all / [test] to run tests: " input
+
+if [ $input -eq 0 ]; then
+    echo "🎉 Running all days 🎈"
+    ./gradlew run --args="-runAll true"
+    exit 0
+elif [[ $input == "test" ]]; then
+    echo "🎉 Running tests 🎈"
+    ./gradlew test
+    exit 0
+else
+    echo "🎉 Running day $input 🎈"
+    ./gradlew run --args="-day $input"
+fi
+
