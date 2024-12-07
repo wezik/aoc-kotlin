9eb0a1e80689c6d14301966c86b30170fbd0014c
Dominik <wezik@users.noreply.github.com>
2024-12-07 16:34:02 +0100

    Day 7 part 1 solution

---
+++ src/main/kotlin/solution/solver/days/Day7Solver.kt
@@ -0,0 +1,67 @@
+package org.example.solution.solver.days
+
+import org.example.solution.solver.Solver
+
+class Day7Solver : Solver {
+
+    private fun interface Instruction {
+        fun value(): Long
+        operator fun plus(other: Instruction) = value() + other.value()
+        operator fun times(other: Instruction) = value() * other.value()
+    }
+
+    private data class Equation(val sum: Long, val values: List<Long>)
+
+    private fun List<String>.parse(): List<Equation> {
+        return this.map { it.split(":") }.map {
+            Equation(sum = it.first().toLong(),
+                values = it.last().split(" ").filter { it.isNotBlank() }.map { it.toLong() })
+        }
+    }
+
+    // Generate all possible combinations for a given list and length
+    fun generateCombinations(strings: List<String>, length: Int): List<List<String>> {
+        if (length == 0) return listOf(emptyList())
+        val smallerCombinations = generateCombinations(strings, length - 1)
+        return smallerCombinations.flatMap { combo ->
+            strings.map { str -> combo + str }
+        }
+    }
+
+    private fun Equation.isValid(): Boolean {
+        // Test all generated combinations of operators
+        val operatorCombos = generateCombinations(listOf("+", "*"), values.size - 1)
+        for (combo in operatorCombos) {
+            if (this.toInstructionOf(combo).value() == sum) return true
+        }
+        return false
+    }
+
+    private fun createInstruction(op: String, a: Instruction, b: Instruction): Instruction {
+        return when (op) {
+            "+" -> Instruction { a + b }
+            "*" -> Instruction { a * b }
+            else -> throw IllegalArgumentException("Unknown operator $op")
+        }
+    }
+
+    private fun Equation.toInstructionOf(operators: List<String>): Instruction {
+        var instruction =
+            createInstruction(operators.first(), Instruction { values.first() }, Instruction { values[1] })
+
+        for (i in 2 until values.size) {
+            instruction = createInstruction(operators[i - 1], instruction, Instruction { values[i] })
+        }
+
+        return instruction
+    }
+
+    override fun part1(input: List<String>): String {
+        val equations = input.parse()
+        return equations.filter { it.isValid() }.map { it.sum }.sum().toString()
+    }
+
+    override fun part2(input: List<String>): String {
+        return "0"
+    }
+}
---
+++ src/test/kotlin/solution/Day7Spec.kt
@@ -0,0 +1,55 @@
+package solution
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import org.example.solution.solver.days.Day7Solver
+
+class Day7Spec : FreeSpec({
+
+    val solver = Day7Solver()
+
+    "\"Find and sum equations that can be made true\" part 1" - {
+        "example input" {
+            // given
+            val input = listOf(
+                "190: 10 19",
+                "3267: 81 40 27",
+                "83: 17 5",
+                "156: 15 6",
+                "7290: 6 8 6 15",
+                "161011: 16 10 13",
+                "192: 17 8 14",
+                "21037: 9 7 18 13",
+                "292: 11 6 16 20",
+            )
+
+            // expect
+            solver.part1(input) shouldBe "3749"
+        }
+        "all + operators" {
+            // given
+            val input = listOf("7: 4 2 1")
+
+            // expect
+            solver.part1(input) shouldBe "7"
+        }
+
+        "all * operators" {
+            // given
+            val input = listOf("16: 4 2 2")
+
+            // expect
+            solver.part1(input) shouldBe "16"
+        }
+        "single * operators" {
+            // given
+            val input = listOf(
+                "18: 8 2 2",
+                "20: 1 9 2",
+            )
+            // expect
+            solver.part1(input) shouldBe "38"
+        }
+    }
+
+})
