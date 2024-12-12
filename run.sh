64853fb99a27154ebdebc0ae72d4c746ef2b22e6
Dominik <wezik@users.noreply.github.com>
2024-12-12 23:54:35 +0100

    Fix 'expected number' warn in runner

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/bash
 
 echo "🎅 Welcome to the advent of code 2024 solver 🎄"
-read -p "Input: [1-24] day / [0] run all / [test] to run tests: " input
+read -p "Input: [1-25] day / [0] run all / [test] to run tests: " input
 
-if [ $input -eq 0 ]; then
-    echo "🎉 Running all days 🎈"
-    ./gradlew run --args="-runAll true"
-    exit 0
-elif [[ $input == "test" ]]; then
+if [[ $input == "test" ]]; then
     echo "🎉 Running tests 🎈"
     ./gradlew test
+    exit 0
+elif [ $input -eq 0 ]; then
+    echo "🎉 Running all days 🎈"
+    ./gradlew run --args="-runAll true"
     exit 0
 else
     echo "🎉 Running day $input 🎈"
