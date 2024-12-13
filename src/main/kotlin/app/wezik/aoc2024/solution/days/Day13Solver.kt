6bca51ea7c2149036057ac985fdb7f22ff3dd8e9
Dominik <wezik@users.noreply.github.com>
2024-12-13 12:08:14 +0100

    Solve day 13 part 2

--- src/main/kotlin/app/wezik/aoc2024/solution/days/Day13Solver.kt
+++ src/main/kotlin/app/wezik/aoc2024/solution/days/Day13Solver.kt
@@ -1,79 +1,52 @@
 package app.wezik.aoc2024.solution.days
 
 import app.wezik.aoc2024.solution.Solver
-import java.util.PriorityQueue
 
 class Day13Solver : Solver {
 
-    private data class Vector(val x: Int, val y: Int)
-
     private operator fun Vector.plus(other: Vector): Vector = Vector(x + other.x, y + other.y)
-
+    private data class Vector(val x: Long, val y: Long)
     private data class State(val destination: Vector, val buttons: List<Pair<Vector, Int>>)
 
-    private fun List<String>.parse(): List<State> {
+    private fun List<String>.parse(fixConversion: Boolean): List<State> {
         val buttonQueue = mutableListOf<Vector>()
         val states = mutableListOf<State>()
-        this.forEach {
-            when (it.firstOrNull()) {
-                null -> return@forEach
-                'B' -> {
-                    val (xString, yString) = it.split(',')
-                    val (_, x) = xString.split('+')
-                    val (_, y) = yString.split('+')
-                    buttonQueue.add(Vector(x.toInt(), y.toInt()))
-                }
+
+        forEach { line ->
+            when (line.firstOrNull()) {
+                'B' -> buttonQueue.add(
+                    line.split(',').let { (xString, yString) ->
+                        Vector(xString.split('+')[1].toLong(), yString.split('+')[1].toLong())
+                    }
+                )
 
                 'P' -> {
-                    val (xString, yString) = it.split(',')
-                    val (_, x) = xString.split('=')
-                    val (_, y) = yString.split('=')
-                    val destination = Vector(x.toInt(), y.toInt())
-                    states.add(
-                        State(destination, listOf(buttonQueue.removeFirst() to 3, buttonQueue.removeFirst() to 1))
-                    )
+                    val offset = if (fixConversion) Vector(10_000_000_000_000L, 10_000_000_000_000L) else Vector(0L, 0L)
+                    val vector = line.split(',').let { (xString, yString) ->
+                        Vector(xString.split('=')[1].toLong(), yString.split('=')[1].toLong()) + offset
+                    }
+                    states.add(State(vector, listOf(buttonQueue.removeFirst() to 3, buttonQueue.removeFirst() to 1)))
                 }
             }
         }
         return states
     }
 
-    private fun List<String>.findCheapestPaths() =
-        this.parse().map { it.findCheapestPath(it.destination, it.buttons) }.filterNotNull().sum()
+    private fun List<String>.findCheapestPaths(fixConversion: Boolean = false) =
+        this.parse(fixConversion).map { it.findCheapestPath(it.destination, it.buttons) }.filterNotNull().sum()
 
-    private data class Path(var cost: Int = 0, var position: Vector = Vector(0, 0))
+    private fun State.findCheapestPath(destination: Vector, buttons: List<Pair<Vector, Int>>): Long? {
+        val vecA = buttons[0].first
+        val vecB = buttons[1].first
 
-    private fun State.findCheapestPath(destination: Vector, buttons: List<Pair<Vector, Int>>): Int? {
-        val pq = PriorityQueue<Path>(compareBy { it.cost })
-        val visited = mutableSetOf<Vector>()
+        val countA = (destination.x * vecB.y - destination.y * vecB.x).toDouble() / (vecA.x * vecB.y - vecA.y * vecB.x)
+        val countB = (destination.x - vecA.x * countA).toDouble() / (vecB.x)
 
-        // Bootstrap
-        pq.add(Path())
-        while (pq.isNotEmpty()) {
-            val current = pq.poll()
-
-            // Reached the destination, return the cost
-            if (current.position == destination) return current.cost
-
-            // Mark as visited
-            if (visited.contains(current.position)) continue
-            visited.add(current.position)
-
-            // Explore all moves
-            for ((move, cost) in buttons) {
-                val newPosition = current.position + move
-                val newCost = current.cost + cost
-
-                if (!visited.contains(newPosition) && newCost <= 400) {
-                    pq.add(Path(newCost, newPosition))
-                }
-            }
-        }
-        // Queue is empty, no path found
-        return null
+        if (countA % 1 != 0.0 || countB % 1 != 0.0) return null
+        return countA.toLong() * 3 + countB.toLong()
     }
 
     override fun part1(input: List<String>) = input.findCheapestPaths().toString()
-    override fun part2(input: List<String>) = ""
+    override fun part2(input: List<String>) = input.findCheapestPaths(fixConversion = true).toString()
 
 }
