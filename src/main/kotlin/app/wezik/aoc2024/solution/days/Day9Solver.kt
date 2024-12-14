237fb7ca2314213a9ca3dc8af65c0faf0636ea2c
Dominik <wezik@users.noreply.github.com>
2024-12-14 11:55:12 +0100

    Refactor day 9 solution

--- src/main/kotlin/app/wezik/aoc2024/solution/days/Day9Solver.kt
+++ src/main/kotlin/app/wezik/aoc2024/solution/days/Day9Solver.kt
@@ -1,109 +1,82 @@
 package app.wezik.aoc2024.solution.days
 
 import app.wezik.aoc2024.solution.Solver
+import kotlin.collections.first
 
 class Day9Solver : Solver {
 
-    private fun List<String>.parse(): MutableList<Int?> {
-        val entries = mutableListOf<Int?>()
-        var memoryIndex = 0
-        for (i in 0 until this.first().length) {
-            val value = this.first()[i].digitToInt()
+    override fun part1(input: List<String>): String {
+        // Parse
+        var fileId = 0
+        val disk = mutableListOf<Int>()
+        for ((i, x) in input.first().withIndex()) {
+            if (i % 2 != 0) repeat(x.digitToInt()) { disk.add(-1) }
+            else {
+                repeat(x.digitToInt()) { disk.add(fileId) }
+                fileId++
+            }
+        }
+
+        // Process
+        val freeSpace = disk.withIndex().filter { it.value == -1 }.map { it.index }
+        for (i in freeSpace) {
+            while (disk.last() == -1) disk.removeLast()
+            if (disk.size <= i) break
+            disk[i] = disk.removeLast()
+        }
+
+        // Check sum
+        return disk.withIndex().map { (i, x) -> (i * x).toLong() }.sum().toString()
+    }
+
+    override fun part2(input: List<String>): String {
+        // Parse
+        var fileId = 0
+        var pos = 0
+        val freeSpace = mutableListOf<Pair<Int, Int>>()
+        val files = mutableMapOf<Int, Pair<Int, Int>>()
+        for ((i, x) in input.first().withIndex()) {
+            val x = x.digitToInt()
             if (i % 2 == 0) {
-                entries.addAll((0 until value).map { memoryIndex })
-                memoryIndex += 1
-            } else {
-                entries.addAll((0 until value).map { null })
+                files[fileId] = pos to x
+                fileId++
+            } else if (x != 0) {
+                freeSpace.add(pos to x)
             }
-        }
-        return entries
-    }
-
-    private fun List<Int?>.debugPrint() {
-        println(this.joinToString(separator = "") { it?.toString() ?: "." })
-    }
-
-    private fun List<Int?>.organizeFreeSpace(): List<Int?> {
-        val organized = this.toMutableList()
-        val (listOfFreeSpaces, listOfTakenSpaces) = organized.withIndex().partition { it.value == null }.let {
-            Pair(it.first.toMutableList(), it.second.toMutableList())
+            pos += x
         }
 
-        var firstFreeSpaceIndex = listOfFreeSpaces.removeFirst().index
-        var lastTakenSpaceIndex = listOfTakenSpaces.removeLast().index
-
-        while (firstFreeSpaceIndex < lastTakenSpaceIndex) {
-            organized.withIndex().find { it.value == null }?.let {
-                organized[it.index] = organized[lastTakenSpaceIndex]
-                organized[lastTakenSpaceIndex] = null
-            }
-            firstFreeSpaceIndex = listOfFreeSpaces.removeFirst().index
-            lastTakenSpaceIndex = listOfTakenSpaces.removeLast().index
-        }
-
-        return organized
-    }
-
-    private fun List<Int?>.organizeFreeSpacePart2(): List<Int?> {
-        val organized = this.toMutableList()
-        val uniqueStarts = this.findUnique()
-
-        for (uniqueStart in uniqueStarts.reversed()) {
-            val takenSize = organized.getSizeFrom(uniqueStart.index)
-            for (i in 0 until uniqueStart.index) {
-                val isFree = organized[i] == null
-                if (!isFree) continue
-                val freeSize = organized.getSizeFrom(i)
-                // Can't reassign `i` to skip empty spaces so it iterates without a need for it...
-                if (freeSize < takenSize) continue
-                for (j in i until i + takenSize) {
-                    val x = uniqueStart.index + (j - i)
-                    organized[j] = organized[uniqueStart.index + (j - i)]
-                    organized[uniqueStart.index + (j - i)] = null
+        // Process
+        while (fileId > 0) {
+            fileId -= 1
+            val (pos, size) = files[fileId]!!
+            for ((i, value) in freeSpace.withIndex()) {
+                val (start, length) = value
+                if (start >= pos) {
+                    freeSpace.subList(i + 1, freeSpace.size).clear()
+                    break
+                }
+                if (size <= length) {
+                    files[fileId] = start to size
+                    if (size == length) {
+                        freeSpace.removeAt(i)
+                    } else {
+                        freeSpace[i] = start + size to length - size
+                    }
+                    break
                 }
             }
         }
 
-        return organized
-    }
-
-    private fun List<Int?>.getSizeFrom(index: Int): Int {
-        val value = this[index]
-        var size = 1
-        var nextValue = this[index + size]
-        while (value == nextValue) {
-            size++
-            if (index + size >= this.size) break
-            nextValue = this[index + size]
-        }
-        return size
-    }
-
-    private fun List<Int?>.findUnique(): List<IndexedValue<Int?>> {
-        val uniqueSet = mutableSetOf<Int>()
-        return this.withIndex().filter {
-            if (it.value != null && uniqueSet.contains(it.value).not()) {
-                uniqueSet.add(it.value!!)
-                return@filter true
+        // Check sum
+        var checkSum = 0L
+        for ((fileId, value) in files) {
+            val (pos, size) = value
+            for (x in (pos until pos + size)) {
+                checkSum += fileId * x
             }
-            return@filter false
         }
-    }
-
-    private fun List<Int?>.checkSum(): Long {
-        return this.withIndex().filter { it.value != null }.map { (it.index * it.value!!).toLong() }.sum()
-    }
-
-    override fun part1(input: List<String>): String {
-        val input = input.parse()
-        val organized = input.organizeFreeSpace()
-        return organized.checkSum().toString()
-    }
-
-    override fun part2(input: List<String>): String {
-        val input = input.parse()
-        val organized = input.organizeFreeSpacePart2()
-        return organized.checkSum().toString()
+        return checkSum.toString()
     }
 
 }
