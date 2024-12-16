8a9c20a5bccb5e8b4aaee038cd2341a75f2892bd
Dominik <wezik@users.noreply.github.com>
2024-12-16 15:55:41 +0100

    Cleanup service for day 16

--- src/main/kotlin/app/wezik/aoc2024/solution/days/Day16.kt
+++ src/main/kotlin/app/wezik/aoc2024/solution/days/Day16.kt
@@ -6,189 +6,118 @@ import java.util.PriorityQueue
 class Day16 : Solver {
 
     private operator fun Pair<Int, Int>.plus(other: Pair<Int, Int>) = first + other.first to second + other.second
+    private data class QueueEntry(val cost: Int, val position: Pair<Int, Int>, val direction: Pair<Int, Int>)
 
     override fun part1(input: List<String>): String {
-        val grid = List(input.size) { BooleanArray(input.first().length) }
-        // start and end seem to be the same on every input
+        // start and end are the same on every input
         var start = 1 to input.size - 2
         var end = input.first().length - 2 to 1
+
+        // prepare the grid
+        val grid = List(input.size) { BooleanArray(input.first().length) }
         for (row in 0 until input.size) {
             for (col in 0 until input[row].length) {
                 if (input[row][col] != '#') grid[row][col] = true
             }
         }
 
-        data class Foo(val cost: Int, val position: Pair<Int, Int>, val direction: Pair<Int, Int>) {
-            override fun toString(): String {
-                return "Foo(Cost: $cost, Position: $position, Direction: $direction)"
-            }
-        }
+        // priority queue based Dijkstra
+        val startEntry = QueueEntry(cost = 0, position = start, direction = 1 to 0)
+        val queue = PriorityQueue<QueueEntry>(compareBy { it.cost }).apply { add(startEntry) }
+        val seen = mutableSetOf(startEntry.position to startEntry.direction)
 
-        val queue = PriorityQueue<Foo>(compareBy { it.cost })
-        queue.add(Foo(0, start, (1 to 0)))
-        val seen = mutableSetOf(start to (1 to 0))
+        var result: Int? = null
+        while (queue.isNotEmpty() && result == null) {
+            val entry = queue.poll()
+            // adding to seen only after processing
+            seen.add(entry.position to entry.direction)
+            if (entry.position == end) result = entry.cost
 
-        fun printBoard(str: String) {
-            val str = buildString {
-                val foo = mutableListOf<String>()
-                for (y in 0 until grid.size) {
-                    for (x in 0 until grid.first().size) {
-                        if (seen.find { it.first == x to y } != null) {
-                            append("██")
-                            seen.filter { it.first == x to y }
-                                .forEach { foo.add("Seen at ${it.first} with direction ${it.second}") }
-                        } else if (grid[y][x]) {
-                            append("  ")
-                        } else {
-                            append("[]")
-                        }
-                    }
-                    append("\n")
-                }
-                append(str)
-            }
-            println("\u001B[1;1H\u001B[2J$str")
-            Thread.sleep(20)
-        }
-
-        // Move to the top right corner of the terminal
-//        println("\\u001B[1;1H")
-
-        while (queue.isNotEmpty()) {
-            val foo = queue.poll()
-            seen.add(foo.position to foo.direction)
-            if (foo.position == end) return foo.cost.toString()
-
-//            val str = buildString {
-//                append("Pulled from queue: ${foo}\n")
-//                append("Next in queue: ${queue.peek()}\n")
-//                append("Queue contains (first 10 elements):\n")
-//                if (queue.size > 10) {
-//                    queue.toList().subList(0, 10).forEach { append("${it}\n") }
-//                    append("... and ${queue.size - 10} more\n")
-//                } else {
-//                    queue.toList().forEach { append("${it}\n") }
-//                }
-//            }
-//            printBoard(str)
+            // next entries
+            val (dx, dy) = entry.direction
+            val ahead = QueueEntry(entry.cost + 1, entry.position + (dx to dy), dx to dy)
+            val right = QueueEntry(entry.cost + 1000, entry.position, -dy to dx)
+            val left = QueueEntry(entry.cost + 1000, entry.position, dy to -dx)
 
             // processing loop
-            val (dx, dy) = foo.direction
-            val ahead = Foo(foo.cost + 1, foo.position + (dx to dy), dx to dy)
-            val right = Foo(foo.cost + 1000, foo.position, -dy to dx)
-            val left = Foo(foo.cost + 1000, foo.position, dy to -dx)
             val candidates = listOf(ahead, right, left)
             for (next in candidates) {
                 val (nx, ny) = next.position
                 if (!grid[ny][nx]) continue
-                if (seen.contains(next.position to next.direction)) continue
+                if (next.position to next.direction in seen) continue
                 queue.add(next)
             }
         }
-        return ""
+
+        return result.toString()
     }
 
     override fun part2(input: List<String>): String {
-        val grid = List(input.size) { BooleanArray(input.first().length) }
-        // start and end seem to be the same on every input
+        // start and end are the same on every input
         var start = 1 to input.size - 2
         var end = input.first().length - 2 to 1
+
+        // prepare the grid
+        val grid = List(input.size) { BooleanArray(input.first().length) }
         for (row in 0 until input.size) {
             for (col in 0 until input[row].length) {
                 if (input[row][col] != '#') grid[row][col] = true
             }
         }
 
-        data class Foo(val cost: Int, val position: Pair<Int, Int>, val direction: Pair<Int, Int>) {
-            override fun toString(): String {
-                return "Foo(Cost: $cost, Position: $position, Direction: $direction)"
-            }
-        }
+        // priority queue based Dijkstra
+        val startEntry = QueueEntry(cost = 0, position = start, direction = 1 to 0)
+        val queue = PriorityQueue<QueueEntry>(compareBy { it.cost }).apply { add(startEntry) }
+        val endStates = HashSet<QueueEntry>()
 
-        val queue = PriorityQueue<Foo>(compareBy { it.cost })
-        queue.add(Foo(0, start, (1 to 0)))
-        val lowestCost = mutableMapOf((start to (1 to 0)) to 0)
-        val backtrack = HashMap<Foo, MutableSet<Foo>?>()
+        // origin tracking
+        val backtrack = HashMap<QueueEntry, MutableSet<QueueEntry>?>()
+        val seenCost = mutableMapOf((startEntry.position to startEntry.direction) to 0)
         var bestCost = Int.MAX_VALUE
-        val endStates = mutableSetOf<Foo>()
-
-//        fun printBoard(str: String) {
-//            val str = buildString {
-//                val foo = mutableListOf<String>()
-//                for (y in 0 until grid.size) {
-//                    for (x in 0 until grid.first().size) {
-//                        if (lowestCost.find { it.first == x to y } != null) {
-//                            append("██")
-//                            lowestCost.filter { it.first == x to y }
-//                                .forEach { foo.add("Seen at ${it.first} with direction ${it.second}") }
-//                        } else if (grid[y][x]) {
-//                            append("  ")
-//                        } else {
-//                            append("[]")
-//                        }
-//                    }
-//                    append("\n")
-//                }
-//                append(str)
-//            }
-//            println("\u001B[1;1H\u001B[2J$str")
-//            Thread.sleep(20)
-//        }
-
-        // Move to the top right corner of the terminal
-//        println("\\u001B[1;1H")
 
         while (queue.isNotEmpty()) {
-            val foo = queue.poll()
-            if (foo.cost > (lowestCost[foo.position to foo.direction] ?: Int.MAX_VALUE)) continue
-            if (foo.position == end) {
-                if (foo.cost > bestCost) break
-                bestCost = foo.cost
-                endStates.add(foo)
+            val entry = queue.poll()
+            // if current entry is worse or present in seen, skip
+            if (entry.cost > (seenCost[entry.position to entry.direction] ?: Int.MAX_VALUE)) continue
+            // at the end pull in all valid end states
+            if (entry.position == end) {
+                if (entry.cost > bestCost) break
+                bestCost = entry.cost
+                endStates.add(entry)
             }
 
-//            val str = buildString {
-//                append("Pulled from queue: ${foo}\n")
-//                append("Next in queue: ${queue.peek()}\n")
-//                append("Queue contains (first 10 elements):\n")
-//                if (queue.size > 10) {
-//                    queue.toList().subList(0, 10).forEach { append("${it}\n") }
-//                    append("... and ${queue.size - 10} more\n")
-//                } else {
-//                    queue.toList().forEach { append("${it}\n") }
-//                }
-//            }
-//            printBoard(str)
+            // next entries
+            val (dx, dy) = entry.direction
+            val ahead = QueueEntry(entry.cost + 1, entry.position + (dx to dy), dx to dy)
+            val right = QueueEntry(entry.cost + 1000, entry.position, -dy to dx)
+            val left = QueueEntry(entry.cost + 1000, entry.position, dy to -dx)
 
             // processing loop
-            val (dx, dy) = foo.direction
-            val ahead = Foo(foo.cost + 1, foo.position + (dx to dy), dx to dy)
-            val right = Foo(foo.cost + 1000, foo.position, -dy to dx)
-            val left = Foo(foo.cost + 1000, foo.position, dy to -dx)
             val candidates = listOf(ahead, right, left)
             for (next in candidates) {
                 val (nx, ny) = next.position
                 if (!grid[ny][nx]) continue
-                val lowest = lowestCost[(nx to ny) to next.direction] ?: Int.MAX_VALUE
+
+                // on lower cost we are purging backtracking map and updating the cost
+                val lowest = seenCost[(nx to ny) to next.direction] ?: Int.MAX_VALUE
                 if (next.cost > lowest) continue
                 if (next.cost < lowest) {
                     backtrack[next] = mutableSetOf()
-                    lowestCost[next.position to next.direction] = next.cost
+                    seenCost[next.position to next.direction] = next.cost
                 }
-                backtrack[next]!!.add(foo)
+
                 queue.add(next)
+                backtrack[next]!!.add(entry)
             }
         }
 
-        val states = ArrayDeque<Foo>()
-        states.addAll(endStates)
-        val seen = HashSet<Foo>()
-        seen.addAll(endStates)
+        // track back to starting point
+        val states = ArrayDeque<QueueEntry>(endStates)
+        val seen = HashSet<QueueEntry>(endStates)
 
         while (states.isNotEmpty()) {
-            val foo = states.removeFirst()
-            val origins = backtrack[foo] ?: emptySet()
-            println("$foo -> $origins")
+            val entry = states.removeFirst()
+            val origins = backtrack[entry] ?: continue
             for (origin in origins) {
                 if (origin in seen) continue
                 seen.add(origin)
@@ -196,25 +125,7 @@ class Day16 : Solver {
             }
         }
 
-        println(endStates)
-
-        val set = seen.map { it.position }.toSet()
-        val str = buildString {
-            for (y in 0 until grid.size) {
-                for (x in 0 until grid.first().size) {
-                    if (set.contains(x to y)) {
-                        append("██")
-                    } else {
-                        append("  ")
-                    }
-                }
-                append("\n")
-            }
-        }
-        println(str)
-        println(set)
-
-        return set.size.toString()
+        return seen.distinctBy { it.position }.size.toString()
     }
 
 }
