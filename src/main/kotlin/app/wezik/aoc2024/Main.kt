cab30285c8cdbc8c26d721cd6bd7b425c9ed38e2
Dominik <wezik@users.noreply.github.com>
2024-12-18 11:56:25 +0100

    CI CD benchmarking

--- src/main/kotlin/app/wezik/aoc2024/Main.kt
+++ src/main/kotlin/app/wezik/aoc2024/Main.kt
@@ -1,10 +1,21 @@
 package app.wezik.aoc2024
 
 import app.wezik.aoc2024.solution.StaticSolverSelector
+import app.wezik.aoc2024.solution.StaticSolverSelector.SolverSource
 import app.wezik.aoc2024.utils.readFrom
+import app.wezik.aoc2024.utils.time
+import app.wezik.aoc2024.utils.toNanoDuration
 import kotlin.time.Duration
 import kotlin.time.DurationUnit
 import kotlin.time.toDuration
+
+// some are just slow unfortunately
+private val hardcodedRunLimits = mapOf(
+    SolverSource.DAY06 to 10,
+    SolverSource.DAY11 to 500,
+    SolverSource.DAY14 to 100,
+    SolverSource.DAY16 to 100,
+)
 
 fun main(args: Array<String>) {
     val argsMap = args.toList().chunked(2).associate { it[0] to it[1] }
@@ -22,31 +33,44 @@ fun main(args: Array<String>) {
     solvers.forEach { solverSource ->
         val inputPath = inputArg ?: solverSource.path
         val input = readFrom(inputPath)
-        if (benchmarkArg != null && benchmarkArg == "true") {
-            println()
-            println("[Day ${solverSource.ordinal + 1}] solver running...")
-            val (p1Time, p2Time) = runBenchmark(solverSource)
-            println(" Part 1 solution ${p1Time.formatToMs()} on average over $benchmarkRuns runs")
-            println(" Part 2 solution ${p2Time.formatToMs()} on average over $benchmarkRuns runs")
+        if (benchmarkArg != null) {
+            var benchmarkRuns = benchmarkArg.toInt()
+            // capping runs for some days to avoid long running benchmarks
+            if (hardcodedRunLimits.containsKey(solverSource)) {
+                benchmarkRuns = minOf(benchmarkRuns, hardcodedRunLimits[solverSource]!!)
+            }
+            val day = if (solverSource.ordinal < 9) "0${solverSource.ordinal + 1}" else solverSource.ordinal + 1
+            val totalTime = time {
+                println("[Day $day]:")
+                val (p1Time, p2Time) = runBenchmark(solverSource, input, benchmarkRuns)
+                println("  Part 1 average: ${p1Time.formatToMs()}")
+                println("  Part 2 average: ${p2Time.formatToMs()}")
+            }.toNanoDuration()
+            println("  Total runs: $benchmarkRuns")
+            println("  Total time: $totalTime (${totalTime.formatToSeconds()})")
             return@forEach
         }
         println()
-        println("[Day ${solverSource.ordinal + 1}] solver running...")
         val solution = solverSource.solver.solve(input)
         println(" Part 1 solution in ${solution.part1.time.formatToSeconds()}: \"${solution.part1.result}\"")
         println(" Part 2 solution in ${solution.part2.time.formatToSeconds()}: \"${solution.part2.result}\"")
     }
 }
 
-val benchmarkRuns = 1000
-
 private fun Duration.formatToSeconds() = this.toString(DurationUnit.SECONDS, 3)
 private fun Duration.formatToMs() = this.toString(DurationUnit.MILLISECONDS, 3)
 
-private fun runBenchmark(source: StaticSolverSelector.SolverSource): Pair<Duration, Duration> {
+private fun runBenchmark(
+    source: StaticSolverSelector.SolverSource,
+    input: List<String>,
+    benchmarkRuns: Int,
+): Pair<Duration, Duration> {
     val p1Times = mutableListOf<Long>()
     val p2Times = mutableListOf<Long>()
-    val input = readFrom(source.path)
+
+    // warmup
+    repeat(5) { source.solver.solve(input) }
+
     (0 until benchmarkRuns).forEach {
         source.solver.solve(input).let {
             p1Times.add(it.part1.time.inWholeNanoseconds)
