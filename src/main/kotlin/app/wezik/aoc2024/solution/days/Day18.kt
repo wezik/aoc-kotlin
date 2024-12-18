5eed23311e140c947361fbae4d321089790a4291
Dominik <wezik@users.noreply.github.com>
2024-12-18 10:45:31 +0100

    Solve day 18 both parts

--- src/main/kotlin/app/wezik/aoc2024/solution/days/Day18.kt
+++ src/main/kotlin/app/wezik/aoc2024/solution/days/Day18.kt
@@ -4,7 +4,87 @@ import app.wezik.aoc2024.solution.Solver
 
 class Day18 : Solver {
 
-    override fun part1(input: List<String>) = ""
-    override fun part2(input: List<String>) = ""
+    private companion object {
+        val DIRECTIONS = listOf(
+            Pair(0, -1), // Up
+            Pair(0, 1),  // Down
+            Pair(-1, 0), // Left
+            Pair(1, 0),  // Right
+        )
+    }
+
+    private data class QueueEntry(val position: Pair<Int, Int>, val cost: Int)
+
+    private fun getDistance(badBytes: List<BooleanArray>, max: Int): Int {
+        // BFS
+        val queue = ArrayDeque<QueueEntry>().apply { add(QueueEntry(0 to 0, 0)) }
+        queue.add(QueueEntry(0 to 0, 0))
+        val seen = HashSet<Pair<Int, Int>>()
+
+        while (queue.isNotEmpty()) {
+            val (pos, dist) = queue.removeFirst()
+            for ((dirX, dirY) in DIRECTIONS) {
+                val (x, y) = pos
+                val nextX = x + dirX
+                val nextY = y + dirY
+                // Out of bounds check
+                if (nextX !in 0..max || nextY !in 0..max) continue
+                // Bad byte check
+                if (badBytes[nextY][nextX]) continue
+                // Seen check
+                if (seen.contains(nextX to nextY)) continue
+                // End check
+                if (nextX == max && nextY == max) return dist + 1
+                seen.add(nextX to nextY)
+                queue.add(QueueEntry(nextX to nextY, dist + 1))
+            }
+        }
+
+        return -1
+    }
+
+    override fun part1(input: List<String>) = part1(input, 1024)
+    fun part1(input: List<String>, cycles: Int): String {
+        var max = 0
+        val badBytesInput = mutableListOf<Pair<Int, Int>>()
+        for ((i, line) in input.withIndex()) {
+            if (i >= cycles) break
+            line.split(",").map { it.toInt() }.let { (x, y) ->
+                // This max tracking is bad, it counts on max value appearing in the n of cycles
+                // but seems to work fine with the inputs
+                if (x > max) max = x
+                if (y > max) max = y
+                badBytesInput.add(x to y)
+            }
+        }
+        // We don't read more bytes than cycle count so all are valid
+        val badBytes = List<BooleanArray>(max + 1) { BooleanArray(max + 1) }
+        badBytesInput.forEach { (x, y) -> badBytes[y][x] = true }
+
+        return getDistance(badBytes, max).toString()
+    }
+
+    override fun part2(input: List<String>): String {
+        var max = 0
+        val badBytesInput = ArrayDeque<Pair<Int, Int>>()
+        for (line in input) {
+            line.split(",").map { it.toInt() }.let { (x, y) ->
+                if (x > max) max = x
+                if (y > max) max = y
+                badBytesInput.add(x to y)
+            }
+        }
+
+        val badBytes = List<BooleanArray>(max + 1) { BooleanArray(max + 1) }
+
+        while (badBytesInput.isNotEmpty()) {
+            val (bx, by) = badBytesInput.removeFirst()
+            badBytes[by][bx] = true
+            val dist = getDistance(badBytes, max)
+            if (dist == -1) return "$bx,$by"
+        }
+
+        return "all valid"
+    }
 
 }
--- src/test/kotlin/solution/Day18Spec.kt
+++ src/test/kotlin/solution/Day18Spec.kt
@@ -7,20 +7,72 @@ import io.kotest.matchers.shouldBe
 class Day18Spec : FreeSpec({
     val solver = Day18()
 
-    "part 1 - " {
+    "part 1 - ram run" {
         // given
-        val input = listOf("")
+        val input = listOf(
+            "5,4",
+            "4,2",
+            "4,5",
+            "3,0",
+            "2,1",
+            "6,3",
+            "2,4",
+            "1,5",
+            "0,6",
+            "3,3",
+            "2,6",
+            "5,1",
+            "1,2",
+            "5,5",
+            "2,5",
+            "6,5",
+            "1,4",
+            "0,4",
+            "6,4",
+            "1,1",
+            "6,1",
+            "1,0",
+            "0,5",
+            "1,6",
+            "2,0",
+        )
 
         // expect
-        solver.part1(input) shouldBe ""
+        solver.part1(input, 12) shouldBe "22"
     }
 
-    "part 2 - " {
+    "part 2 - which byte will block the exit" {
         // given
-        val input = listOf("")
+        val input = listOf(
+            "5,4",
+            "4,2",
+            "4,5",
+            "3,0",
+            "2,1",
+            "6,3",
+            "2,4",
+            "1,5",
+            "0,6",
+            "3,3",
+            "2,6",
+            "5,1",
+            "1,2",
+            "5,5",
+            "2,5",
+            "6,5",
+            "1,4",
+            "0,4",
+            "6,4",
+            "1,1",
+            "6,1",
+            "1,0",
+            "0,5",
+            "1,6",
+            "2,0",
+        )
 
         // expect
-        solver.part2(input) shouldBe ""
+        solver.part2(input) shouldBe "6,1"
     }
 
 })
