58ce881896ee602ed39f80ff8893d3a43fe741a2
Dominik <wezik@users.noreply.github.com>
2024-12-19 11:51:42 +0100

    Solve day 19 both parts

--- src/main/kotlin/app/wezik/aoc2024/solution/days/Day19.kt
+++ src/main/kotlin/app/wezik/aoc2024/solution/days/Day19.kt
@@ -1,10 +1,49 @@
 package app.wezik.aoc2024.solution.days
 
 import app.wezik.aoc2024.solution.Solver
+import kotlin.math.min
 
 class Day19 : Solver {
 
-    override fun part1(input: List<String>) = ""
-    override fun part2(input: List<String>) = ""
+    override fun part1(input: List<String>): String {
+        val patterns = input[0].split(", ").toSet()
+        val maxLength = patterns.maxOf { it.length }
+
+        val cache = mutableMapOf("" to true)
+        fun canObtain(design: String): Boolean {
+            if (design in cache) return cache[design]!!
+            for (i in 0..min(design.length, maxLength)) {
+                if (design.substring(0, i) in patterns && canObtain(design.substring(i))) {
+                    cache[design] = true
+                    return true
+                }
+            }
+            cache[design] = false
+            return false
+        }
+
+        return input.drop(2).filter { canObtain(it) }.count().toString()
+    }
+
+    override fun part2(input: List<String>): String {
+        val patterns = input[0].split(", ").toSet()
+        val maxLength = patterns.maxOf { it.length }
+
+        val cache = mutableMapOf("" to 1L)
+        fun possibilities(design: String): Long {
+            if (design in cache) return cache[design]!!
+            var count = 0L
+            if (design in cache) return cache[design]!!
+            for (i in 0..min(design.length, maxLength)) {
+                if (design.substring(0, i) in patterns) {
+                    count += possibilities(design.substring(i))
+                }
+            }
+            cache[design] = count
+            return count
+        }
+
+        return input.drop(2).map { possibilities(it) }.sum().toString()
+    }
 
 }
--- src/test/kotlin/solution/Day19Spec.kt
+++ src/test/kotlin/solution/Day19Spec.kt
@@ -7,20 +7,42 @@ import io.kotest.matchers.shouldBe
 class Day19Spec : FreeSpec({
     val solver = Day19()
 
-    "part 1 - " {
+    "part 1 - check possible linen designs" {
         // given
-        val input = listOf("")
+        val input = listOf(
+            "r, wr, b, g, bwu, rb, gb, br",
+            "\n",
+            "brwrr",
+            "bggr",
+            "gbbr",
+            "rrbgbr",
+            "ubwu",
+            "bwurrg",
+            "brgr",
+            "bbrgwb",
+        )
 
         // expect
-        solver.part1(input) shouldBe ""
+        solver.part1(input) shouldBe "6"
     }
 
-    "part 2 - " {
+    "part 2 - count possible linen designs" {
         // given
-        val input = listOf("")
+        val input = listOf(
+            "r, wr, b, g, bwu, rb, gb, br",
+            "\n",
+            "brwrr",
+            "bggr",
+            "gbbr",
+            "rrbgbr",
+            "ubwu",
+            "bwurrg",
+            "brgr",
+            "bbrgwb",
+        )
 
         // expect
-        solver.part2(input) shouldBe ""
+        solver.part2(input) shouldBe "16"
     }
 
 })
