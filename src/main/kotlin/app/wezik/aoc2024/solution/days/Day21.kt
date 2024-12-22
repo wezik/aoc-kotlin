1de7adff3e58d868bf781f0f02e7275ee1cf3502
Dominik <wezik@users.noreply.github.com>
2024-12-22 15:23:00 +0100

    Day 21 benchmark improvements

--- src/main/kotlin/app/wezik/aoc2024/solution/days/Day21.kt
+++ src/main/kotlin/app/wezik/aoc2024/solution/days/Day21.kt
@@ -36,19 +36,22 @@ class Day21 : Solver {
 
     // pre-computed numeric keypad sequences
     private val numSequence = findAllSequences(Keypad.NUMERIC)
+
     // pre-computed directional keypad sequences
     private val dirSequence = findAllSequences(Keypad.DIRECTIONAL)
+
     // cache of the shortest length between two directional buttons
     private val dirCache = dirSequence.entries.map { (it.key to it.value.first().length.toLong()) }.toMap()
 
-    // entry point!!! code is a mess but it somehow reads from top to bottom
+    // entry point!!! code is a mess, but it somehow reads from top to bottom
     override fun part1(input: List<String>) = common(input, 2)
     override fun part2(input: List<String>) = common(input, 25)
     private fun common(input: List<String>, depth: Int): String {
         var total = 0L
+        solve = ::solveWorker.memoize() // reset cache for benchmarks
         for (line in input) {
             val inputs = solveNumpad(line, numSequence)
-            val length = inputs.map { solveMemoized(it, depth) }.min()
+            val length = inputs.map { solve(it, depth) }.min()
             total += length * line.substring(0 until line.length - 1).toLong()
         }
         return total.toString()
@@ -62,12 +65,13 @@ class Day21 : Solver {
     }
 
     // calculating and storing only the length of the shortest possibles sequence
-    private val solveMemoized = ::solve.memoize()
-    private fun solve(sequence: String, depth: Int): Long {
+    // note: tried MemoizedDeepRecursiveFunction, but it's annoying to set up and achieves the same thing
+    private var solve = ::solveWorker.memoize()
+    private fun solveWorker(sequence: String, depth: Int): Long {
         if (depth == 1) return "A$sequence".zip(sequence).sumOf { (a, b) -> dirCache[a to b]!! }
         var length = 0L
         for ((a, b) in "A$sequence".zip(sequence)) {
-            length += dirSequence[a to b]!!.map { solveMemoized(it, depth - 1) }.min()
+            length += dirSequence[a to b]!!.map { solve(it, depth - 1) }.min()
         }
         return length
     }
@@ -108,7 +112,7 @@ class Day21 : Solver {
         return possible
     }
 
-    private fun Pair<Int,Int>.neighbourCandidates(): List<Triple<Int, Int, Char>> {
+    private fun Pair<Int, Int>.neighbourCandidates(): List<Triple<Int, Int, Char>> {
         val (x, y) = this
         return listOf(Triple(x - 1, y, '<'), Triple(x + 1, y, '>'), Triple(x, y - 1, '^'), Triple(x, y + 1, 'v'))
     }
