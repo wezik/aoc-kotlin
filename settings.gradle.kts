d69883d201b93995bdb9f110e6c19585cfa5c900
Dominik <wezik@users.noreply.github.com>
2025-07-05 13:47:05 +0200

    Simplify gradle toolchain resolution

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,8 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
 }
+
 rootProject.name = "advent-of-code"
 
