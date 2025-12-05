0b9996770fc48e2d9278abd4af72e7aabf7b7ed0
Dominik <wezik@users.noreply.github.com>
2025-12-05 23:12:42 +0100

    2025 Day 1 Part 2

--- src/main/kotlin/app/wezik/aoc/solutions/y2025/Day01.kt
+++ src/main/kotlin/app/wezik/aoc/solutions/y2025/Day01.kt
@@ -5,6 +5,7 @@ import app.wezik.aoc.domain.SolutionInput
 import app.wezik.aoc.domain.SolutionResult
 import app.wezik.aoc.domain.SolutionResult.NotImplemented
 import kotlin.math.abs
+import kotlin.math.absoluteValue
 
 object Day01 : Solution() {
 
@@ -26,16 +27,44 @@ object Day01 : Solution() {
 
         for (distance in inputs) {
             dial += distance
-            when {
-                dial > 0 -> while (dial >= 100) dial -= 100
-                dial < 0 -> while (dial < 0) dial += 100
-            }
-
+            while (dial > 99) dial -= 100
+            while (dial < 0) dial += 100
             if (dial == 0) sum++
         }
 
         return SolutionResult.Success(sum)
     }
 
-    override fun part2(input: SolutionInput) = NotImplemented
+    override fun part2(input: SolutionInput): SolutionResult {
+        val inputs = input.lines
+            .filter { it.length > 1 }
+            .map {
+                val distance = it.subSequence(1, it.length).toString().toInt()
+                when (it[0]) {
+                    'L' -> -distance
+                    'R' -> distance
+                    else -> throw IllegalArgumentException("invalid direction")
+                }
+            }
+
+        // NOTE: Dial starts by pointing at 50
+        var dial = 50
+        var sum = 0
+
+        for (distance in inputs) {
+            val rot = distance / 100
+            val rem = distance % 100
+
+            sum += rot.absoluteValue
+
+            when {
+                dial != 0 && dial + rem <= 0 -> sum++
+                dial + rem >= 100 -> sum++
+            }
+
+            dial = (dial + rem + 100) % 100
+        }
+
+        return SolutionResult.Success(sum)
+    }
 }
