2749878d3bc02ee5c2e56776ab285838ed7052c9
Dominik <wezik@users.noreply.github.com>
2026-01-03 17:56:48 +0100

    Day 11 2025 Part 2 solution

--- src/main/kotlin/app/wezik/aoc/solutions/y2025/Day11.kt
+++ src/main/kotlin/app/wezik/aoc/solutions/y2025/Day11.kt
@@ -8,7 +8,6 @@ import java.util.BitSet
 object Day11 : Solution() {
 
     override fun part1(input: SolutionInput): SolutionResult {
-
         val (points, connections) = input.lines.map { line ->
             val (point, connections) = line.split(": ")
             point to connections.split(" ")
@@ -32,9 +31,7 @@ object Day11 : Solution() {
         while (queue.isNotEmpty()) {
             val current = queue.removeFirst()
             if (current == end) {
-                // purge the queue out of ends at once
-                paths += queue.count { it == end }
-                queue.removeAll { it == end }
+                paths++
                 continue
             }
 
@@ -46,7 +43,51 @@ object Day11 : Solution() {
         return SolutionResult.Success(paths)
     }
 
-    // override fun part2(input: SolutionInput): SolutionResult {
-    //     return SolutionResult.Success(0)
-    // }
+    // NOTE: Unfortunately current runner structure does not allow for different inputs depending on the part of the puzzle so this will print incorrect results for test runs
+    override fun part2(input: SolutionInput): SolutionResult {
+        val (points, connections) = input.lines.map { line ->
+            val (point, connections) = line.split(": ")
+            point to connections.split(" ")
+        }.let { entries ->
+            val points = entries.map { (point, _) -> point }.toSet().toList()
+            val connections = List<MutableList<Int>>(points.size) { mutableListOf() }
+            for ((point, connected) in entries) {
+                val pointIndex = points.indexOf(point)
+                connections[pointIndex].addAll(connected.map { points.indexOf(it) }.toList())
+            }
+
+            points to connections
+        }
+
+        val start = points.indexOf("svr")
+        val end = -1 // as it's not listed in the input as a point, it will be identifiable by -1
+        val fft = points.indexOf("fft")
+        val dac = points.indexOf("dac")
+
+        val cache = Array(points.size) { LongArray(4) { -1 } }
+
+        // mask 0 = none, 1 = fft, 2 = dac, 3 = both
+        fun findOuts(index: Int, mask: Int): Long {
+            val newMask = 
+                mask or
+                (if (index == fft) 1 else 0) or
+                (if (index == dac) 2 else 0)
+
+            if (index == end) return if (newMask == 3) 1 else 0
+
+            if (cache[index][newMask] != -1L) return cache[index][newMask]
+
+            var total = 0L
+            for (value in connections[index]) {
+                total += findOuts(value, newMask)
+            }
+
+            cache[index][newMask] = total
+            return total
+        }
+
+        val paths = findOuts(start, 0)
+
+        return SolutionResult.Success(paths)
+    }
 }
